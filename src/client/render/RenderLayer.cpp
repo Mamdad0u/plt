0f4725e97f2858fb3645a80884b21f660a46e997
Paullgk <paul-le-guen@outlook.fr>
2021-11-12 23:04:16 +0100

    Passage version release

--- src/client/render/RenderLayer.cpp
+++ src/client/render/RenderLayer.cpp
@@ -51,7 +51,7 @@ namespace render {
     void render::RenderLayer::LoadUI(){
         
         mUI.CreateWindow(0,500,3200,100);
-        mUI.SetTextVersion("Beta V2.1");
+        mUI.SetTextVersion("Release V2.1");
         mUI.DEBUG_SetTextAction("MENU ACTION");
     }
 
