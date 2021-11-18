6a5435b6a42245f25adec94965cff1b835a7f32b
Paullgk <paul-le-guen@outlook.fr>
2021-11-18 21:33:16 +0100

    Ajout constructeur classe Action

--- src/shared/state/Action.cpp
+++ src/shared/state/Action.cpp
@@ -1,7 +1,11 @@
 #include <state/Action.h>  // Included from library shared_static
 #include "Action.h"
 namespace state {
+    state::Action::Action(){
+        mAttackList.reserve(2);
+        mSpellList.reserve(2);
 
+    }
 
 
 }
