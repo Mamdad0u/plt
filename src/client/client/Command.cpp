0e66b6fdd05e36fb6cedd40279f06dbd95bfdfdf
Paullgk <paul-le-guen@outlook.fr>
2021-11-19 15:55:37 +0100

    Implement ComputeWeakAndStrength function

--- src/client/client/Command.cpp
+++ src/client/client/Command.cpp
@@ -1,11 +1,106 @@
 #include <client/Command.h>  // Included from library shared_static
 #include "Command.h"
 
+using namespace state;
 
 namespace client {
     float client::Command::ComputeWeakAndStrength(state::Major rAttackerMajor, state::Major rVictimMajor){
+        
+        switch(rAttackerMajor){
+            case ELEC:
+                switch (rVictimMajor)
+                {
+                case ELEC:
+                    return NEUTRAL;
 
+                case INFO:
+                    return NEUTRAL;
 
+                case AUTO:
+                    return WEAK;
+
+                case SIGNAL:
+                    return STRENGTH;
+
+                case SCIENCES_HUMAINES:
+                    return NEUTRAL;
+
+                }
+                break;
+            
+            case INFO:
+                switch (rVictimMajor)
+                {
+                case ELEC:
+                    return NEUTRAL;
+
+                case INFO:
+                    return NEUTRAL;
+
+                case AUTO:
+                    return STRENGTH;
+
+                case SIGNAL:
+                    return WEAK;
+
+                case SCIENCES_HUMAINES:
+                    return NEUTRAL;
+                }
+                break;
+
+            case AUTO:
+                switch (rVictimMajor)
+                {
+                case ELEC:
+                    return STRENGTH;
+
+                case INFO:
+                    return WEAK;
+
+                case AUTO:
+                    return NEUTRAL;
+
+                case SIGNAL:
+                    return NEUTRAL;
+
+                case SCIENCES_HUMAINES:
+                    return NEUTRAL;
+
+                }
+                break;
+
+            case SIGNAL:
+                switch (rVictimMajor)
+                {
+                case ELEC:
+                    return WEAK;
+
+                case INFO:
+                    return STRENGTH;
+
+                case AUTO:
+                    return NEUTRAL;
+
+                case SIGNAL:
+                    return NEUTRAL;
+
+                case SCIENCES_HUMAINES:
+                    return NEUTRAL;
+                }
+                break;
+
+            case SCIENCES_HUMAINES:
+                switch (rVictimMajor)
+                {
+                case SCIENCES_HUMAINES:
+                    return STRENGTH;
+                
+
+                default: // All other major
+                    return NEUTRAL;
+                }
+                break;
+        }
         
     }
 
