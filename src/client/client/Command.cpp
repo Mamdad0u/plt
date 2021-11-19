c55c76b0a7ba06751e83fd9d4daaab18a5e895fb
Paullgk <paul-le-guen@outlook.fr>
2021-11-19 15:16:37 +0100

    Création du fichier

---
+++ src/client/client/Command.cpp
@@ -0,0 +1,13 @@
+#include <client/Command.h>  // Included from library shared_static
+#include "Command.h"
+
+
+namespace client {
+    float client::Command::ComputeWeakAndStrength(state::Major rOpponentMajor_1, state::Major rOpponentMajor_2){
+        
+
+        
+    }
+
+
+}
