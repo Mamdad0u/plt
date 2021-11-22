d8977fad9502ef3ad427be4c7bb6b52654a2f580
Paullgk <paul-le-guen@outlook.fr>
2021-11-22 11:12:22 +0100

    Add name compatibility

--- test/client/test_client_dummy.cpp
+++ test/client/test_client_dummy.cpp
@@ -2,6 +2,9 @@
 #include <boost/test/unit_test.hpp>
 
 #include <SFML/Graphics.hpp>
+#include "client/Engine.h"
+
+using namespace ::client;
 
 BOOST_AUTO_TEST_CASE(TestStaticAssert)
 {
@@ -16,4 +19,19 @@ BOOST_AUTO_TEST_CASE(TestSFML)
   }
 }
 
-/* vim: set sw=2 sts=2 et : */
+BOOST_AUTO_TEST_CASE(TestEngine){
+  {
+    Engine UUT_Engine;
+
+    BOOST_CHECK_EQUAL(UUT_Engine.DEBUG_GetGameStatus(), state::IN_COMBAT);
+
+    
+  }
+
+
+
+
+}
+
+
+
