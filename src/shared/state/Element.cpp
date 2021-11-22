07b6d7fcfcab4a714dd10d051ed1b898bffc2225
Paullgk <paul-le-guen@outlook.fr>
2021-11-22 11:10:14 +0100

    Rename name attribute

--- src/shared/state/Element.cpp
+++ src/shared/state/Element.cpp
@@ -4,26 +4,26 @@ namespace state {
 
 
 state::Element::Element(){
-    this->nName = DEFAULT_NAME;
+    this->mName = DEFAULT_NAME;
     
 }
 
 
 state::Element::Element (std::string rName, int rX, int rY) {
-    this->nName = rName;
+    this->mName = rName;
     this->setMX(rX);
     this->setMY(rY);
 }
 
 std::string state::Element::GetName() {
-    return this->nName;
+    return this->mName;
 }
 
-const std::string& state::Element::getNName() const {
+const std::string& state::Element::getMName() const {
     
 }
 
-void state::Element::setNName(const std::string& nName) {
+void state::Element::setMName(const std::string& nName) {
     
 }
 
