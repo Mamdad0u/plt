7160fc5a9d75df8d79f638109617fccdc908d8d9
Abdelmajid-Khames <abdelmajid.khames@gmail.com>
2021-11-22 13:47:07 +0100

    Affichage du Character Evil

--- src/client/render/RenderLayer.cpp
+++ src/client/render/RenderLayer.cpp
@@ -9,6 +9,7 @@ namespace render {
 
     render::RenderLayer::RenderLayer(){
         mPlayerCharactersSurface.reserve(4);
+        mArenaEnemySurface.reserve(4);
 
     }
     
@@ -21,23 +22,25 @@ namespace render {
         return 0;
     }
 
-    int render::RenderLayer::LoadEnemy(int rEnemySelected,int rX, int rY){
-        
-        string lEnemyString = "Character" + to_string(rEnemySelected) + "Evil";
+    int render::RenderLayer::LoadEnemy(int rEnemySelected,int rX, int rY, int rSide){
+        Surface lEnemytoAdd;
+        int lLastEnemyPosition=mArenaEnemySurface.size();
+        string lEnemyString = "Character" + to_string(rEnemySelected);
         
         lEnemyString = lEnemyString +".png";
+        mArenaEnemySurface.push_back(*(new Surface));
 
-        if(mEnemySurface.LoadCharacterSprite(lEnemyString, rX, rY)){
+        if(mArenaEnemySurface[lLastEnemyPosition].LoadCharacterSprite(lEnemyString, rX, rY, rSide)){
             cout << "ERROR : Failed to load character " << endl;
             return -1;
         }
-        mEnemySurface.SetCharacterAnimation(0);
+        mArenaEnemySurface[lLastEnemyPosition].SetCharacterAnimation(0);
 
         return 0;
 
     }
 
-    int render::RenderLayer::LoadCharacter(int rCharacterSelected, int rX, int rY){
+    int render::RenderLayer::LoadCharacter(int rCharacterSelected, int rX, int rY, int rSide){
         Surface lCharactertoAdd;
         int lLastCharacterPosition = mPlayerCharactersSurface.size();
         string lCharacterString = "Character" + to_string(rCharacterSelected);
@@ -48,7 +51,7 @@ namespace render {
         
         mPlayerCharactersSurface.push_back(*(new Surface));
         
-        if(mPlayerCharactersSurface[lLastCharacterPosition].LoadCharacterSprite(lCharacterString, rX, rY)){
+        if(mPlayerCharactersSurface[lLastCharacterPosition].LoadCharacterSprite(lCharacterString, rX, rY, rSide)){
             cout << "ERROR : Failed to load character " << endl;
             return -1;
         }
@@ -95,12 +98,16 @@ namespace render {
 
     void render::RenderLayer::AnimateCharacters(){
         int lLastCharacterPosition = mPlayerCharactersSurface.size();
+        int lLastEnemyPosition = mArenaEnemySurface.size();
 
         for(int i=0;i<lLastCharacterPosition;i++){
-            mPlayerCharactersSurface[i].UpdateCharacterAnimation();
+            mPlayerCharactersSurface[i].UpdateCharacterAnimation(1);
         }
 
-        mEnemySurface.UpdateCharacterAnimation();
+        
+        for(int j=0;j<lLastEnemyPosition;j++){
+            mArenaEnemySurface[j].UpdateCharacterAnimation(0);
+        }
 
     }
 
@@ -109,33 +116,35 @@ namespace render {
 
 
         for(int i=0;i<lLastCharacterPosition;i++){
-            if(mMovingProgress == 0){
+            if(mMovingProgress%800!=0){
+               // cout << "L'animation devient mobile" << endl;
                 mPlayerCharactersSurface[i].SetCharacterAnimation(1);
             }
-            else if(mMovingProgress == 799){
+            else if(mMovingProgress%800==0){
+                //cout << "L'animation devient statique" << endl;
                 mPlayerCharactersSurface[i].SetCharacterAnimation(0);
                 
             }
 
             mPlayerCharactersSurface[i].MoveCharacterSprite();
+            mArenaEnemySurface[i].MoveCharacterSprite();
         }
-        mEnemySurface.MoveCharacterSprite();
 
 
-        mBackgroundSurface.MoveBackgroundView(rWindow);
+        mBackgroundSurface.MoveBackgroundView(rWindow, mMovingProgress);
         
         mUI.MoveUI();
+      
         mMovingProgress++;
-        return mMovingProgress;
+        return (mMovingProgress-1);
     }
 
-    void render::RenderLayer::draw(sf::RenderWindow& rWindow){
+    void render::RenderLayer::draw(sf::RenderWindow& rWindow, int rEnemyIndex, state::State GameStatus){
         mBackgroundSurface.DrawSprite(rWindow);
 
-        if(mMovingProgress==800){
-            mEnemySurface.DrawSprite(rWindow);
+        if(GameStatus.GetCombatState()==state::IN_COMBAT){
+            mArenaEnemySurface[rEnemyIndex].DrawSprite(rWindow);
         }
-      
         
 
         for(int i=0;i<mPlayerCharactersSurface.size();i++){
