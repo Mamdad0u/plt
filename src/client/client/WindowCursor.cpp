4757fa3433c6620e789c790799b703e49af33a58
Abdelmajid-Khames <abdelmajid.khames@gmail.com>
2021-11-23 11:16:32 +0100

    Classe du Curseur

---
+++ src/client/client/WindowCursor.cpp
@@ -0,0 +1,101 @@
+#include <client/WindowCursor.h>  // Included from library shared_static
+#include "WindowCursor.h"
+#include <iostream>
+
+using namespace std;
+
+namespace client {
+    client::WindowCursor::WindowCursor(){
+        mX=0;
+        mY=0;
+    }
+
+    void client::WindowCursor::GetPositionCursor(sf::RenderWindow& rWindow){
+        sf::Vector2i lLocalPosition;
+        lLocalPosition= sf::Mouse::getPosition(rWindow);
+        mX=lLocalPosition.x;
+        mY=lLocalPosition.y;
+    }
+
+    bool client::WindowCursor::Click(sf::RenderWindow& rWindow){
+        sf::Vector2i lLocalPosition;
+        lLocalPosition= sf::Mouse::getPosition(rWindow);
+        mX=lLocalPosition.x;
+        mY=lLocalPosition.y;
+
+        if((mX<800)&&(mX>0)&&(mY<600)&&(mY>500)){
+            if( sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+                return true;
+            }
+        }
+        
+        return false;
+        
+    }
+
+    bool client::WindowCursor::ClickAction1(sf::RenderWindow& rWindow){
+        sf::Vector2i lLocalPosition;
+        lLocalPosition= sf::Mouse::getPosition(rWindow);
+        mX=lLocalPosition.x;
+        mY=lLocalPosition.y;
+
+        if((mX<120)&&(mX>35)&&(mY<535)&&(mY>510)){
+            if( sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+                return true;
+            }
+        }
+        
+        return false;
+        
+    }
+
+    bool client::WindowCursor::ClickAction2(sf::RenderWindow& rWindow){
+        sf::Vector2i lLocalPosition;
+        lLocalPosition= sf::Mouse::getPosition(rWindow);
+        mX=lLocalPosition.x;
+        mY=lLocalPosition.y;
+
+        if((mX<500)&&(mX>0)&&(mY<600)&&(mY>500)){
+            if( sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+                return true;
+            }
+        }
+        
+        return false;
+        
+    }
+
+    bool client::WindowCursor::ClickAction3(sf::RenderWindow& rWindow){
+        sf::Vector2i lLocalPosition;
+        lLocalPosition= sf::Mouse::getPosition(rWindow);
+        mX=lLocalPosition.x;
+        mY=lLocalPosition.y;
+
+        if((mX<800)&&(mX>0)&&(mY<600)&&(mY>500)){
+            if( sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+                return true;
+            }
+        }
+        
+        return false;
+        
+    }
+    bool client::WindowCursor::ClickAction4(sf::RenderWindow& rWindow){
+        sf::Vector2i lLocalPosition;
+        lLocalPosition= sf::Mouse::getPosition(rWindow);
+        mX=lLocalPosition.x;
+        mY=lLocalPosition.y;
+
+        if((mX<800)&&(mX>0)&&(mY<600)&&(mY>500)){
+            if( sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+                return true;
+            }
+        }
+        
+        return false;
+        
+    }
+
+
+
+}
