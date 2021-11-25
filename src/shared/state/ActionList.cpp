c69c08ac9373e1d2c0de8666eb558fb233354b5b
Paullgk <paul-le-guen@outlook.fr>
2021-11-25 22:02:25 +0100

    Fix bad index value & list init

--- src/shared/state/ActionList.cpp
+++ src/shared/state/ActionList.cpp
@@ -2,16 +2,23 @@
 #include "ActionList.h"
 namespace state {
     state::ActionList::ActionList(){
+        Action lNewAction;
+
         mAttackList.reserve(3);
         mSpellList.reserve(3);
 
+        for(int i=0;i<MAX_CHARACTER_ACTION;i++){
+            mAttackList.push_back(lNewAction);
+            mSpellList.push_back(lNewAction);
+        }
+        
     }
 
     Action* state::ActionList::GetAttack (int rAttackNumber){
 
         Action* lReturnAttack = nullptr;
 
-        lReturnAttack = &mAttackList[rAttackNumber];
+        lReturnAttack = &mAttackList[rAttackNumber-1];
         return lReturnAttack;
 
     }
@@ -19,22 +26,22 @@ namespace state {
     Action* state::ActionList::GetSpell (int rSpellNumber){
         Action* lReturnSpell = nullptr;
 
-        lReturnSpell = &mAttackList[rSpellNumber];
+        lReturnSpell = &mAttackList[rSpellNumber-1];
         return lReturnSpell;
     }
 
     void state::ActionList::SetAttack(int rAttackNumber, int rDamageValue, StatsName rStatsBuffName, int rStatsBuffValue, bool rBeneficial){
-        this->mAttackList[rAttackNumber].SetDamage(rDamageValue);
-        this->mAttackList[rAttackNumber].SetStatBuffName(rStatsBuffName);
-        this->mAttackList[rAttackNumber].SetStatBuffValue(rStatsBuffValue);
-        this->mAttackList[rAttackNumber].SetBuffBeneficial(rBeneficial);
+        this->mAttackList[rAttackNumber-1].SetDamage(rDamageValue);
+        this->mAttackList[rAttackNumber-1].SetStatBuffName(rStatsBuffName);
+        this->mAttackList[rAttackNumber-1].SetStatBuffValue(rStatsBuffValue);
+        this->mAttackList[rAttackNumber-1].SetBuffBeneficial(rBeneficial);
     }
 
     void state::ActionList::SetSpell(int rSpellNumber, int rDamageValue, StatsName rStatsBuffName, int rStatsBuffValue, bool rBeneficial){
-        this->mSpellList[rSpellNumber].SetDamage(rDamageValue);
-        this->mSpellList[rSpellNumber].SetStatBuffName(rStatsBuffName);
-        this->mSpellList[rSpellNumber].SetStatBuffValue(rStatsBuffValue);
-        this->mSpellList[rSpellNumber].SetBuffBeneficial(rBeneficial);
+        this->mSpellList[rSpellNumber-1].SetDamage(rDamageValue);
+        this->mSpellList[rSpellNumber-1].SetStatBuffName(rStatsBuffName);
+        this->mSpellList[rSpellNumber-1].SetStatBuffValue(rStatsBuffValue);
+        this->mSpellList[rSpellNumber-1].SetBuffBeneficial(rBeneficial);
     }
 
 
