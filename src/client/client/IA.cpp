746023afbb1bce9e497d4bacd604e27ce645981d
Paullgk <paul-le-guen@outlook.fr>
2021-11-29 09:54:55 +0100

    Add random command generation

--- src/client/client/IA.cpp
+++ src/client/client/IA.cpp
@@ -1,7 +1,15 @@
 #include <client/IA.h>  // Included from library shared_static
 #include "IA.h"
+#include <stdio.h>      /* printf, scanf, puts, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 
 namespace client {
+    client::IA::IA(){
+        srand (time(NULL));
+    }
+
+
     void client::IA::SetStatusCommand(CommandID NewCommand){
         mInputCommand = NewCommand;
         NotifyNewPlayerCommand();
@@ -9,5 +17,15 @@ namespace client {
 
     CommandID client::IA::GetStatusCommand () const{
         return this->mInputCommand;
+    }
+
+    void client::IA::GenerarateRandomCommand(){
+        CommandID lCommandIDTab[] = {ATTACK_1, ATTACK_2, SPELL_1, SPELL_2}; 
+        int lRandomInt = 0;
+        static CommandID lRandomCommand;
+
+        lRandomInt = rand() % 3;
+        lRandomCommand = lCommandIDTab[lRandomInt];
+        SetStatusCommand(lRandomCommand);
     }
 }
