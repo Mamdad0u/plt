d9a359d40ab317b797e76546e5cfda65b9bd0a7f
Paullgk <paul-le-guen@outlook.fr>
2021-12-08 22:32:45 +0100

    New stress test on 1000 execution

--- test/stress_test.sh
+++ test/stress_test.sh
@@ -1,3 +1,9 @@
-for run in {1..100}; do
-./build/test/client/test_client_dummy
+for run in {1..1000}; do
+./bin/client test;
+
+
+if [ $? -gt 0 ]; then
+echo "Test failed, program crashed" 
+exit 128
+fi
 done
