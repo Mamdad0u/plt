c28b9f57904f96dbce0695d5a25b6394333af8c7
Paullgk <paul-le-guen@outlook.fr>
2022-01-04 20:14:03 +0100

    Working implementation of game state tree in UpdateNodeTree()

--- src/client/ai/DeepAI.cpp
+++ src/client/ai/DeepAI.cpp
@@ -13,8 +13,9 @@ using namespace std;
 namespace ai {
 
     ai::DeepAI::DeepAI(){ 
-       mNodeNumber.reserve(5); 
-       mNodeNumber.push_back(1); //Root of the tree
+        mNodeTree.reserve(85); // Arbitrary
+        mNodeNumber.reserve(85); // Arbitrary      
+        mNodeNumber.push_back(1); //Root of the tree
     }
 
 /**
@@ -25,14 +26,14 @@ namespace ai {
     void ai::DeepAI::UpdateNodeTree(int rDepth){
         
         int lNode_Number = 0; // Number of nodes created at the depth tested
+        int lNode_Sum = 0; // Sum of nodes created
         state::ActionListCommand lAction_Type[] = {state::ATTACK_1, state::ATTACK_2, state::SPELL_1, state::SPELL_2};
         
         EngineObserver* lRunningEngine;// Actual game engine running the game
             
         lRunningEngine = *(mEngineObserverList.begin());  // Get parameters and state of game engine
        
-        //lTestEngine = lRunningEngine;
-        // SandBoxEngine* lTestEngine; // Test engine in sandbox environnement to test action
+
         
         State lGameStatus = lRunningEngine->mCurrentState; // Get the state of the game
         
@@ -42,11 +43,16 @@ namespace ai {
         Action* lActiveCharacter_Action = nullptr; 
 
         
-        SandBoxEngine lTestEngine(lGameStatus);
+        SandBoxEngine lTestEngine(lGameStatus);  // Test engine in sandbox environnement to test action
 
-        Node lPrimary_Root_Node(lGameStatus, 0); // FIrst node of the tree
+        Node lPrimary_Root_Node(lGameStatus, 0); // First node of the tree
         lPrimary_Root_Node.SetIndex(0);
-        Node lRoot_Node;
+
+        mNodeTree.push_back(&lPrimary_Root_Node);
+        lNode_Sum++;
+
+
+        Node lParent_Node;
 
         if(lPlayerTurn == IA_MIN_TURN){ // Si c'est le tour de l'IA joueur  de jouer
             lActiveCharacter = lGameStatus.GetActivePlayerCharacter(); // Récupération du character actif de l'IA joueur en train d'être joué
@@ -59,13 +65,14 @@ namespace ai {
         }
         
         
-        for(int lIndex_Depth=0;lIndex_Depth<=rDepth;lIndex_Depth++){ // Building tree over max rDepth
-            
+        for(int lIndex_Depth=0;lIndex_Depth<rDepth;lIndex_Depth++){ // Building tree over max rDepth
+
             for(int lIndex_Node_Number = 0; lIndex_Node_Number<mNodeNumber[lIndex_Depth]; lIndex_Node_Number++){ // Each node present at the depth beeing computed
                 
+
                 if(lIndex_Depth > 0){ // If not the primary root node
-                    lRoot_Node = *(lPrimary_Root_Node.GetBranch(lIndex_Node_Number));
-                    lGameStatus = *(lRoot_Node.GetGameContext()); // Get the game context of the node to test his childs nodes
+                    lParent_Node = *(mNodeTree[(lNode_Sum-1) - lIndex_Node_Number]); // Getting each parent present ath the depth
+                    lGameStatus = *(lParent_Node.GetGameContext()); // Get the game context of the parent node to test his childs nodes
                     lTestEngine.SetGameContext(lGameStatus); // Set Test engine to node game context
                     
                     lPlayerTurn = lGameStatus.GetPlayerStatus();
@@ -84,31 +91,35 @@ namespace ai {
                 }
 
                 else{ // Only for primary root node
-                   lRoot_Node = lPrimary_Root_Node;
+                   lParent_Node = lPrimary_Root_Node;
                 }
                 
                 
+                if(lParent_Node.GetNodeValue() != (int)NODE_GAME_OVER){ // If parent node is not a GAME_OVER node
+                    for(int lIndex_Character_Action = 0; lIndex_Character_Action<4;lIndex_Character_Action++){ // Each character have 4 max action
+                        lActiveCharacter_Action = lActiveCharacter->GetAction(lAction_Type[lIndex_Character_Action]); // Get each action of the active character beeing played
 
-                for(int lIndex_Character_Action = 0; lIndex_Character_Action<4;lIndex_Character_Action++){ // Each character have 4 max action
-                    lActiveCharacter_Action = lActiveCharacter->GetAction(lAction_Type[lIndex_Character_Action]); // Get each action of the active character beeing played
-
-                    if(lActiveCharacter_Action->GetDamage() != 0){ // If action exists on the character (case where a charcter dont have an action specified (ex SPELL_2, etc))
-                        Node* lNewNode = new Node;
-                        *(lNewNode) = lTestEngine.TestAction(*(lActiveCharacter), *(lActiveEnemyCharacter), lAction_Type[lIndex_Character_Action]); //Test the next game state if we execute this action in a new node
-                        lNewNode->SetIndex(lIndex_Depth+1); // The depth of the node defined his index
-                        lRoot_Node.AddBranch(lNewNode); // Add the node created in a branch to the root node 
-                        lTestEngine.SetGameContext(lGameStatus); // And reset game context in root node for next action to test
-                        lNode_Number++;
+                            if((lActiveCharacter_Action->GetDamage() != 0)){ // If action exists on the character (case where a charcter dont have an action specified (ex SPELL_2, etc)),  
+                                Node* lNewChildNode = new Node;
+                                *(lNewChildNode) = lTestEngine.TestAction(lAction_Type[lIndex_Character_Action]); //Test the next game state if we execute this action in a new node
+                                lNewChildNode->SetIndex(lIndex_Depth+1); // The depth of the node defined his index
+                                lParent_Node.AddBranch(lNewChildNode); // Add the node created in a branch to the root node 
+                                lTestEngine.SetGameContext(lGameStatus); // And reset game context in root node for next action to test
+                                lNode_Number++;
+                                mNodeTree.push_back(lNewChildNode); // Add the child node to the parent tree,, so he can become a parent at depth + 1
+                            }
                     }
                 }
+
                 
             }
-
+            /*Necessary to get the primary root node of the tree*/
             if(lIndex_Depth == 0){
-                lPrimary_Root_Node = lRoot_Node;
+                lPrimary_Root_Node = lParent_Node;
             }
 
             mNodeNumber.push_back(lNode_Number); // Add the number of nodes at the depth + 1
+            lNode_Sum += lNode_Number;
             lNode_Number = 0; // And reset it for next depth
 
 
@@ -116,6 +127,23 @@ namespace ai {
         }
     }
 
+
+    void ai::DeepAI::UpdateNodeTree_TEST(int rDepth){
+
+        state::ActionListCommand lAction_Type[] = {state::ATTACK_1, state::ATTACK_2, state::SPELL_1, state::SPELL_2};
+        Node lRoot_Node;
+        EngineObserver* lRunningEngine;// Actual game engine running the game
+        lRunningEngine = *(mEngineObserverList.begin());  // Get parameters and state of game engine
+        State lGameStatus = lRunningEngine->mCurrentState; // Get the state of the game
+
+
+
+
+
+
+
+
+    }
 
     /**
      * @brief Return a command based on minimax algorithm and node tree generated 
@@ -129,7 +157,8 @@ namespace ai {
         State lGameStatus = (*lRunningEngine)->mCurrentState; // Récupération de l'état du jeu depuis l'engine
         Player_Status lPlayerTurn = lGameStatus.GetPlayerStatus();
 
-        UpdateNodeTree(3);
+        UpdateNodeTree(6);
+        cout << "Finished" << endl;
 
 /*         if(lPlayerTurn == IA_MAX_TURN){
 
