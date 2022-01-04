d64eecde5ee990d7c65cd9c4a24b644d7d7d102f
Paullgk <paul-le-guen@outlook.fr>
2022-01-04 08:33:49 +0100

    Fix a bad enum value CommandID != ActionListCommand causing crash on deepAI

--- src/client/ai/DeepAI.cpp
+++ src/client/ai/DeepAI.cpp
@@ -64,9 +64,9 @@ namespace ai {
 
                     if(lActiveCharacter_Action->GetDamage() != 0){ // If action exists on the character (case where a charcter dont have an action specified (ex SPELL_2, etc))
                         Node* lNewNode = new Node;
-                        *(lNewNode) = lTestEngine.TestAction(*(lActiveCharacter), *(lActiveEnemyCharacter), lAction_Type[lIndex_Character_Action]); //Test the next game state if we execute this action
-                        Root_Node.AddBranch(lNewNode); // Add branch to the root node 
-                        lTestEngine.SetGameContext(lGameStatus); // And reset game context to game context in root node 
+                        *(lNewNode) = lTestEngine.TestAction(*(lActiveCharacter), *(lActiveEnemyCharacter), lAction_Type[lIndex_Character_Action]); //Test the next game state if we execute this action in a new node
+                        Root_Node.AddBranch(lNewNode); // Add the node created in a branch to the root node 
+                        lTestEngine.SetGameContext(lGameStatus); // And reset game context in root node for next action to test
 
                     }
                 }
