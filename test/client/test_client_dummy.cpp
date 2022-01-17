68bf2922cec883e82d563a6e53ad090004b6365d
Abdelmajid-Khames <abdelmajid.khames@gmail.com>
2022-01-17 10:16:15 +0100

    TestUnitaire

--- test/client/test_client_dummy.cpp
+++ test/client/test_client_dummy.cpp
@@ -1,17 +1,20 @@
 #include <boost/test/unit_test.hpp>
 
 #include <SFML/Graphics.hpp>
-#include "client.h"
+#include "engine.h"
+#include "ai.h"
+#include "render.h"
 
-
-using namespace ::client;
+using namespace ::engine;
 using namespace ::state;
+using namespace ::ai;
+using namespace ::render;
 
 const float STRENGTH     = 2;
 const float  NEUTRAL     = 1;
 const float WEAK     = 0.5;
 
-BOOST_AUTO_TEST_SUITE( test_client_dummy1 )
+BOOST_AUTO_TEST_SUITE( test_engine_dummy1 )
 
 
 BOOST_AUTO_TEST_CASE(TestStaticAssert)
@@ -27,94 +30,74 @@ BOOST_AUTO_TEST_CASE(TestSFML)
   }
 }
 
-BOOST_AUTO_TEST_CASE(TestCommand){
-  {
-/*     Engine UUT_Engine;
-    state::Character UUT_Character_IS("IS", state::INFO, state::ALIVE);
-    state::Character UUT_Character_SIA("SIA", state::SIGNAL, state::ALIVE);
 
-    UUT_Character_IS.SetCharacterStats(LIFE_POINTS, 80);
-    UUT_Character_IS.SetCharacterStats(ATTACK, 140);
-    UUT_Character_IS.SetCharacterStats(POWER, 140);
-    UUT_Character_IS.SetCharacterStats(DEFENSE, 80);
-    UUT_Character_IS.SetCharacterStats(LUCK, 5);
-    UUT_Character_IS.SetCharacterAction(state::ATTACK_1, 80, LUCK, 0, true);
-    UUT_Character_IS.SetCharacterAction(state::ATTACK_2, 60, POWER, 20, true);
-    UUT_Character_IS.SetCharacterAction(state::SPELL_1, 80, POWER, 0, true);
-    UUT_Character_IS.SetCharacterAction(state::SPELL_2, 60, ATTACK, 20, true);
-
-
-
-
-    UUT_Character_SIA.SetCharacterStats(LIFE_POINTS, 110);
-    UUT_Character_SIA.SetCharacterStats(ATTACK, 80);
-    UUT_Character_SIA.SetCharacterStats(POWER, 150);
-    UUT_Character_SIA.SetCharacterStats(DEFENSE, 60);
-    UUT_Character_SIA.SetCharacterStats(LUCK, 3);
-    UUT_Character_SIA.SetCharacterAction(state::ATTACK_1, 60, LUCK, 3, false); 
-    UUT_Character_SIA.SetCharacterAction(state::ATTACK_2, 80, LUCK, 0, true); 
-
-    UUT_Character_SIA.SetCharacterAction(state::SPELL_1, 80, LUCK, 0, true); 
-    UUT_Character_SIA.SetCharacterAction(state::SPELL_2, 60, LUCK, 3, true); 
-
-    BOOST_CHECK_EQUAL(UUT_Engine.DEBUG_GetGameStatus(), state::IN_COMBAT);
- */
-
-
-
-    //UUT_Engine.mCommand.ComputeAction(*(&UUT_Character_IS), *(&UUT_Character_SIA), CommandID::ATTACK_2);
-
-    
-    
-
-/*     for(int i = 0; i<2;i++){
-      //UUT_Engine.mCommand.ComputeCriticalHit(UUT_Character_IS.GetCharacterStats(LUCK));
-      UUT_Engine.mCommand.ComputeAction(*(&UUT_Character_SIA), *(&UUT_Character_IS), CommandID::ATTACK_1);
-      UUT_Engine.mCommand.ComputeAction(*(&UUT_Character_IS), *(&UUT_Character_SIA), CommandID::ATTACK_1);
-      
-    } */
-     
-
-  }
-
-
-
-
-}
-
-/* BOOST_AUTO_TEST_CASE(TestEngine){
+BOOST_AUTO_TEST_CASE(TestRandomAI){
   {
   Engine NewEngine;
-  
-  IA IA1;
-  IA IA2;
-  JSON_tools tools;
-  Character test;
+  RenderLayer lRender(&NewEngine);
+  RandomAI IA1;
+  RandomAI IA2;
+  State* Game_State;
   CombatStatus GameStatus = INITIALISATION;
   int turn = 0;
 
 
   IA1.AddEngineObserver(&NewEngine);
   IA2.AddEngineObserver(&NewEngine);
-
-  while(GameStatus != GAME_OVER){
-
-    if(turn%2 == 0 && GameStatus == IN_COMBAT){
-        IA1.GenerarateRandomCommand();
-    }
-
-    else if(GameStatus == IN_COMBAT){
-        IA2.GenerarateRandomCommand();
-    }
-    turn++;
-    GameStatus = NewEngine.GameLoop();
-  }
+  lRender.NotifyEndRendering();
+  IA1.GenerarateRandomCommand();
+  Game_State = NewEngine.GameLoop();
   
-  NewEngine.GameLoop();
- 
+
  
   }
-} */
+
+}
+
+BOOST_AUTO_TEST_CASE(TestHeuristicIA){
+  {
+  Engine NewEngine;
+  RenderLayer lRender(&NewEngine);
+  HeuristicAI IA1;
+  HeuristicAI IA2;
+  State* Game_State;
+  CombatStatus GameStatus = INITIALISATION;
+  int turn = 0;
+
+
+  IA1.AddEngineObserver(&NewEngine);
+  IA2.AddEngineObserver(&NewEngine);
+  lRender.NotifyEndRendering();
+  IA1.GenerateHeuristicCommand();
+  Game_State = NewEngine.GameLoop();
+  IA2.GenerateHeuristicCommand();
+
+ 
+  }
+
+}
+BOOST_AUTO_TEST_CASE(TestDeepIA){
+  {
+  Engine NewEngine;
+  RenderLayer lRender(&NewEngine);
+  DeepAI IA1(5);
+  DeepAI IA2(5);
+  State* Game_State;
+  CombatStatus GameStatus = INITIALISATION;
+  int turn = 0;
+
+
+  IA1.AddEngineObserver(&NewEngine);
+  IA2.AddEngineObserver(&NewEngine);
+  lRender.NotifyEndRendering();
+  IA1.GenerateDeepCommand(5);
+  Game_State = NewEngine.GameLoop();
+  IA2.GenerateDeepCommand(5);
+
+ 
+  }
+
+}
 
 /* ******* TEST UNITAIRE DES CLASSES DU RENDER ********** */
 
@@ -122,7 +105,7 @@ BOOST_AUTO_TEST_CASE(TestRenderLayer){
   {
     RenderLayer lRender;
     sf::RenderWindow window(sf::VideoMode(800, 600, 32), "ENSEAi");
-    static int lArena_Number;
+    int lArena_Number = 1;
     lRender.LoadBackground(lArena_Number);
 
     lRender.LoadCharacter(1,250,250,1);
@@ -158,9 +141,9 @@ BOOST_AUTO_TEST_CASE(NewTestSurface){
     sf::RenderWindow window;
 
 
-    lSurface.LoadShape("Arena1.png",0,0);
-    lSurface.LoadCharacterSprite("Character1.png",0,0,0);
-    lSurface.LoadBackgroundSprite("Arena2.png");
+    lSurface.LoadShape("backgrounds/Arena1.png",0,0);
+    lSurface.LoadCharacterSprite("sprites/Character1.png",0,0,0);
+    lSurface.LoadBackgroundSprite("backgrounds/Arena2.png");
     lSurface.SetCharacterAnimation(0);
     lSurface.UpdateCharacterAnimation(1);
 
@@ -244,7 +227,7 @@ BOOST_AUTO_TEST_CASE(TestCommand){
 
 
     int valeur_exacte=27;
-    /*
+    
     if (PVLost>50){
       
       BOOST_CHECK_EQUAL(PVLost,valeur_exacte*2);
@@ -252,7 +235,7 @@ BOOST_AUTO_TEST_CASE(TestCommand){
     if(PVLost<50){
       BOOST_CHECK_EQUAL(PVLost,valeur_exacte);
     }
-    */
+    
 
    int i;
    int count=0;
@@ -274,18 +257,15 @@ BOOST_AUTO_TEST_CASE(TestCommand){
 }
 
 BOOST_AUTO_TEST_CASE(TestPlayer){
-  {
   Player lPlayer;
   sf::RenderWindow window; 
 
   lPlayer.SetStatusCommand(CommandID::ATTACK_1);
 
   BOOST_CHECK_EQUAL(lPlayer.GetStatusCommand(),CommandID::ATTACK_1);
-  }
+
 }
 
 
-
-BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE_END()
